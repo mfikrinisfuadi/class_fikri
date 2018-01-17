497a4c9b664222235ce1429a73ce90da3b48714c
mfikrinisfuadi <acengfikry@gmail.com>
2018-01-17 23:08:01 +0800

    disini saya membuat info tentang hamster dan mengoutput hasilnya

--- tugas1.cpp
+++ tugas1.cpp
@@ -47,3 +47,19 @@ public:
         cout << endl;
     };
     };
+    
+    int main()
+
+    { 
+	Jenishamster HamsterA("Hamster Syrian", "Golden Hamster, Dwarf Campbell's Russian Hamster", 
+	"Cricetinae Phodopus Sungorua campbelli","Berkoloni", 11, 2);
+	
+	 Jenishamster HamsterB("Hamster Cambel", "Golden Hamster, Teddy Bear Hamster", 
+	"Cericetinae Mesocricetus auratus","Soliter", 16, 3);
+
+    HamsterA.perolehInfo();
+    
+    HamsterB.perolehInfo();
+    
+    return 0;
+}
