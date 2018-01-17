fdcf5a2055811dc1496167b6aefb3cf30ce4c79a
mfikrinisfuadi <acengfikry@gmail.com>
2018-01-17 23:03:28 +0800

    disini saya membuat public untuk variable pembentuk yang digunakan untuk menampilkan output

--- tugas1.cpp
+++ tugas1.cpp
@@ -13,3 +13,19 @@ private:
     string karakter;
     int panjangtubuh;
     int batasumur;
+    
+public:
+
+
+    Jenishamster (string namahamster, string namalain, string namalatin, string karakter, 
+	int panjangtubuh, int batasumur )
+
+    {
+    	Jenishamster::namahamster = namahamster;
+        Jenishamster::namalain = namalain;
+        Jenishamster::namalatin = namalatin;
+        Jenishamster::karakter = karakter;
+        Jenishamster::panjangtubuh = panjangtubuh;
+        Jenishamster::batasumur = batasumur;
+        
+    }
