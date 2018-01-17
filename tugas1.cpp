c1e3772def463bd16cf19d5f0d575b8ae68329eb
mfikrinisfuadi <acengfikry@gmail.com>
2018-01-17 23:00:33 +0800

    disini saya membuat private untuk variable class

--- tugas1.cpp
+++ tugas1.cpp
@@ -3,3 +3,13 @@
 using namespace std;
 
 class Jenishamster
+
+{
+private:
+
+	string namahamster;
+    string namalain;
+    string namalatin;
+    string karakter;
+    int panjangtubuh;
+    int batasumur;
